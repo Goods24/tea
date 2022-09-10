65023ee8b8f3aa829044178f36619d428e96f494
Goods24 <112106404+Goods24@users.noreply.github.com>
2022-09-09 21:36:00 -0600

    Add files via upload

---
+++ lab02/lab02-cadena.py
@@ -0,0 +1,9 @@
+#Tendencias e Innovacion en Tecnologias Agricolas (TEA)
+
+x = "Hello world"
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
---
+++ lab02/lab02-imprimirnombre.py
@@ -0,0 +1,7 @@
+nombre = input("Cual es su nombre? ")
+print("Su nombre es Diego")
+edad = input("Cual es su edad? ")
+calculo =int(18)
+print(calculo)
+
+
