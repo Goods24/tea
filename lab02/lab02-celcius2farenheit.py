f0abe7599f44ec0b1cc43a8397368a39ec01ef73
Goods24 <112106404+Goods24@users.noreply.github.com>
2022-09-11 22:45:09 -0600

    Add files via upload

---
+++ lab02/lab02-celcius2farenheit.py
@@ -0,0 +1,9 @@
+message = "Cual es la temperatura en celsius?"
+print(message)
+Valor1 = input("25°C")
+celsius = 25
+
+message = "Valor en Fahrenheit:"
+print(message)
+
+print((celsius * 9 / 5) + 32)
---
+++ lab02/lab02-pagoporhora.py
@@ -0,0 +1,9 @@
+message = input("Cuantas horas trabajo?")
+
+valor = "valor por hora: $13"
+Horas = input(11)
+Costo = 13
+
+print(valor)
+
+print(11 * 13)
